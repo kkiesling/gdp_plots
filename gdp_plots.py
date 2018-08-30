e3b6418d1b74f20d45b9439925c4e0ce051c544e
Kalin Kiesling <krkiesling@gmail.com>
2018-08-30 15:49:39 -0500

    refactored into functions

--- gdp_plots.py
+++ gdp_plots.py
@@ -5,38 +5,77 @@ import matplotlib.pyplot as plt
 import sys
 import glob
 
-# load data and transpose so that country names are
-# the columns and their gdp data becomes the rows
+def parse_arguments():
+    """Parses the user's command line arguments and returns
+    the appropriate list of files.
 
-# read data into a pandas dataframe and transpose
-if len(sys.argv) == 1:
-    # no arguments supplied
-    print("Not enough arguments have been provided")
-    print("Usage: python gdp_plots.py <filenames>")
-    print("Options: -a: plot all gdp data in current directory.")
-    exit()
+    Input:
+    ------
+       Nothing
 
-if sys.argv[1] == '-a' :
-   file_list = glob.glob("*gdp*.csv")
-   if len(file_list) == 0:
-       print("No files found in current directory.")
-       exit()
-else:
-   file_list = sys.argv[1:]
+    Returns:
+    --------
+       file_list: list of filenames (strings)
+    """
+    if len(sys.argv) == 1:
+        # no arguments supplied
+        print("Not enough arguments have been provided")
+        print("Usage: python gdp_plots.py <filenames>")
+        print("Options: -a: plot all gdp data in current directory.")
+        exit()
 
-for filename in file_list:
-    data = pandas.read_csv(filename, index_col = 'country').T
+    if sys.argv[1] == '-a' :
+        file_list = glob.glob("*gdp*.csv")
+        if len(file_list) == 0:
+            print("No files found in current directory.")
+            exit()
+    else:
+        file_list = sys.argv[1:]
 
-    # create a plot the transposed data
-    ax = data.plot(title=filename)
+    return file_list
 
-    # axes labels
-    ax.set_xlabel('Year')
-    ax.set_ylabel('GDP Per Capita')
+def create_plots(file_list):
+    """Plot all files in file_list using Pandas and Matplotlib.
+    Each data file is its own plot.
 
-    # set axes ticks
-    ax.set_xticks(range(len(data.index)))
-    ax.set_xticklabels(data.index, rotation=45)
+    Input:
+    ------
+       file_list: list of files to plot, filenames are strings
 
-    # display the plot
-    plt.show()
+    Return:
+    -------
+       Nothing
+    """
+    # load data and transpose so that country names are
+    # the columns and their gdp data becomes the rows
+
+    # read data into a pandas dataframe and transpose
+    for filename in file_list:
+        data = pandas.read_csv(filename, index_col = 'country').T
+
+        # create a plot the transposed data
+        ax = data.plot(title=filename)
+
+        # axes labels
+        ax.set_xlabel('Year')
+        ax.set_ylabel('GDP Per Capita')
+
+        # set axes ticks
+        ax.set_xticks(range(len(data.index)))
+        ax.set_xticklabels(data.index, rotation=45)
+
+        # display the plot
+        plt.show()
+
+def main():
+    file_list = parse_arguments()
+    create_plots(file_list)
+
+
+
+main()
+
+
+
+
+
